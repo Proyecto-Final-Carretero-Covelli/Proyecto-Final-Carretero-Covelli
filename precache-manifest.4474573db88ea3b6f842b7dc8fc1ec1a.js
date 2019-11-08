03574710dacf57293b884d986c6e68b45684205d
Tomás Covelli <tomas@MacBook-Pro-de-Tomas.local>
2019-11-08 16:46:06 -0300

    Updates

---
+++ precache-manifest.4474573db88ea3b6f842b7dc8fc1ec1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0d0fc0e47aa6547926bad392f8f363",
+    "url": "/Proyecto-Final-Carretero-Covelli/index.html"
+  },
+  {
+    "revision": "c8bb130e5c91ccb48b84",
+    "url": "/Proyecto-Final-Carretero-Covelli/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6d48398b4840472f799e",
+    "url": "/Proyecto-Final-Carretero-Covelli/static/js/2.28d5f345.chunk.js"
+  },
+  {
+    "revision": "c8bb130e5c91ccb48b84",
+    "url": "/Proyecto-Final-Carretero-Covelli/static/js/main.12e65437.chunk.js"
+  },
+  {
+    "revision": "a3f0e3963f2eda4ae6e3",
+    "url": "/Proyecto-Final-Carretero-Covelli/static/js/runtime-main.54f46f82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Proyecto-Final-Carretero-Covelli/static/media/logo.25bf045c.svg"
+  }
+]);
